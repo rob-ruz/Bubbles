f205db789acee17a9c26ed69b6022b3ddb860292
Roberto Ruz <roberto.ruz@outlook.com>
2020-08-17 10:02:00 -0500

    avance

--- nuevo_comienzo.R
+++ nuevo_comienzo.R
@@ -3,6 +3,9 @@ library(readr)
 library(slider)
 library(dplyr)
 library(ggplot2)
+library(tsibble)
+library(tidyr)
+library(readxl)
 
 datos <- read_csv(
     file = "Compustat_Global_Daily.csv",
@@ -22,61 +25,69 @@ fundamentals <- read_csv(
   na = c("N/A", "")
 )
 
-primary <- fundamentals %>%
-    group_by(gvkey, conm) %>%
-    distinct(prirow) %>%
-    drop_na() %>%
-    ungroup() %>%
-    unite(gvkey_iid, gvkey, prirow, remove = FALSE)
-
 sector <-  read_xlsx(
   path = "Sectores_GICS_BMV.xlsx",
   sheet = "Sectores",
-  range = "A1:D292",
+  range = "A1:B292",
   col_names = TRUE,
   col_types = c("text")
-)[, 1:2]
-
-datos <- left_join(datos, sector, by = "gvkey")
-datos <- mutate(datos, adjusted_price = (prccd / ajexdi) * trfd)
-
-consumer_2000 <- filter(
-    datos,
-    sector == "Consumer Staples",
-    datadate >= "2000-01-01",
-    curcdd == "MXN",
-    monthend == 1
 )
 
-consumer_all <- filter(
-    datos,
-    sector == "Consumer Staples",
-    curcdd == "MXN" | curcdd == "MXP",
-    monthend == 1
-)
+fundamentals %>%
+    distinct(gvkey, prirow) %>%
+    drop_na() %>%
+    unite(gvkey_iid, gvkey, prirow)  %>%
+    pull() -> primary
+
+datos %>%
+    filter(
+        curcdd %in% c("MXN", "MXP"),
+        monthend == 1
+    ) -> datos_mth
+
+datos_mth %>%
+    mutate(
+        adjusted_price = (prccd / ajexdi) * trfd,
+        sector = vlookup(gvkey, sector, "gvkey", "sector"),
+        prccd = if_else(prccd == "MXP", prccd / 1000, prccd),
+        date = yearmonth(datadate)
+    ) -> datos_mth
+
+datos_mth %>%
+    filter(
+        sector == "Consumer Staples"
+    ) -> consumer_all
+
+consumer_all %>%
+    filter(
+        datadate >= "2000-01-01"
+    ) -> consumer_2000
+
 
 consumer_2000 <- unite(consumer_2000, gvkey_iid, gvkey, iid, remove = FALSE)
 consumer_all <- unite(consumer_all, gvkey_iid, gvkey, iid, remove = FALSE)
 
-consumer_2000 <- consumer_2000 %>% filter(gvkey_iid %in% fundamentals$gvkey_iid)
-
-prueba %>%
+consumer_all %>%
+    group_by(gvkey) %>%
+    group_modify(~ if (!any(unique(.x$gvkey_iid) %in% primary) & n_distinct(.x$iid) > 1) {
+        group_by(.x,iid) %>% mutate(freq=n()) %>% ungroup() %>% filter(freq == max(freq))
+    } else if (any(unique(.x$gvkey_iid) %in% primary) & n_distinct(.x$iid) > 1) {
+        filter(.x, gvkey_iid %in% primary)
+    } else {
+        .x
+    }) %>%
     ungroup() %>%
     ggplot(aes(datadate, adjusted_price, color = conm)) +
-    geom_point() +
+    geom_line() +
     facet_wrap(~conm, nrow = 8, ncol = 5) +
     theme(legend.position = "None")
 
-prueba <- consumer_all %>%
-    group_by(gvkey) %>%
-    group_modify(~ if (n_distinct(.x$iid) > 1) {
-        filter(.x, gvkey_iid %in% primary$gvkey_iid)
-    } else {
-        .x
-    })
 
 tsibble(mutate(prueba, date = yearmonth(datadate)), key = gvkey_iid, index = date) %>% count_gaps()
 
+
+
+# prueba de runups
 bimbo <- filter(
     datos,
     conm == "GRUPO BIMBO SA DE CV",
@@ -112,4 +123,10 @@ bimbo <- mutate(
 bimbo %>%
     ggplot(aes(date, cum_ret)) +
     geom_line(color = "dodgerblue") +
-    geom_hline(yintercept = 0)
+    geom_hline(yintercept = 0)}
+
+    
+vlookup <- function(this, df, key, value) {
+  m <- match(this, df[[key]])
+  df[[value]][m]
+}
